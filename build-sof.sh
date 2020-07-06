14aa04a601c10f1449d00daf6eb937c0d079e75e
Diana Cretu <dianacretu2806@gmail.com>
2020-07-06 11:16:40 +0300

    Add build script for i.MX8, i.MX8X and i.MX8M
    Signed-off-by: Diana Cretu <diana.cretu@nxp.com>

--- build-sof.sh
+++ build-sof.sh
@@ -7,6 +7,8 @@ export PATH=${SOF_PREFIX}/xtensa-byt-elf/bin/:$PATH
 export PATH=${SOF_PREFIX}/xtensa-hsw-elf/bin/:$PATH
 export PATH=${SOF_PREFIX}/xtensa-apl-elf/bin/:$PATH
 export PATH=${SOF_PREFIX}/xtensa-cnl-elf/bin/:$PATH
+export PATH=${SOF_PREFIX}/xtensa-imx-elf/bin/:$PATH
+export PATH=${SOF_PREFIX}/xtensa-imx8m-elf/bin/:$PATH
 
 cd ${SOF_PREFIX}
 cd crosstool-ng
@@ -24,6 +26,10 @@ cp config-apl-gcc8.1-gdb8.1 .config
 ./ct-ng build
 cp config-cnl-gcc8.1-gdb8.1 .config
 ./ct-ng build
+cp config-imx-gcc9.2-gdb8.3 .config
+./ct-ng build
+cp config-imx8m-gcc9.2-gdb8.3 .config
+./ct-ng build
 cp -r builds/* ${SOF_PREFIX}
 
 cd ${SOF_PREFIX}/newlib-xtensa
@@ -40,6 +46,14 @@ make
 make install
 rm etc/config.cache
 ./configure --target=xtensa-cnl-elf --prefix=${SOF_PREFIX}/xtensa-root
+make
+make install
+rm etc/config.cache
+./configure --target=xtensa-imx-elf --prefix=${SOF_PREFIX}/xtensa-root
+make
+make install
+rm etc/config.cache
+./configure --target=xtensa-imx8m-elf --prefix=${SOF_PREFIX}/xtensa-root
 make
 make install
 
